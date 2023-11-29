b808cb250bbbfee534abd304288dcc376753a8c6
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-11-29 22:09:00 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const API_KEY = 'live_eVFX0N3DgZf8XV7EZFhS9HUEtK1lVasKqGqyQWPZoGDlSDnCwXDvn4ocqwmdZP1e'
+const API_LINK = 'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME'
